647022e7f87684c5242580ce4acc2a21959e57a5
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-11 21:39:32 +0000

    melhor usar toggle

--- buttons.cs
+++ buttons.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class buttons : MonoBehaviour
 {
-   public Button meuBotao;
-    private float tempoInicial;
+    public Button meuBotao;
 
     void Start()
     {
        
     }
 
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Space)){
-            OnDeselect();
+    void Update(){
+         if (meuBotao != null)
+        {
+            // Adiciona este script como um handler para o evento OnPointerUp do botão
+            meuBotao.onClick.AddListener(OnPointerUp);
+
+            // Agende a chamada automática do OnPointerUp após 10 segundos
+            Invoke("ChamarOnPointerUp", 10f);
         }
-       
     }
 
-    public void OnDeselect(){
-        Debug.Log("Descelected");
+    void ChamarOnPointerUp()
+    {
+        OnPointerUp(); // Chama o método após 10 segundos
+    }
+
+    public void OnPointerUp()
+    {
+        Debug.Log("Botão específico foi liberado!");
+        // Adicione aqui o código que deseja executar quando o botão específico for liberado.
     }
 }
