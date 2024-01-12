2497661a862529393afc2eb489ee6fd612dbc2b2
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-12 23:43:52 +0000

    ,

--- quaternion.cs
+++ quaternion.cs
@@ -10,7 +10,7 @@ public class quaternion : MonoBehaviour
     public SerialPort serialPort;
 
     [Header("Serial Unity-Arduino")]
-     //...Carte de amor, que será recebido do arduino,, com certas informações, interprete cada informação do seu jeito e use ela como quiser.
+     //...Carte de amor, que será recebido do arduino, com certas informações, interprete cada informação do seu jeito e use ela como quiser.
     public string mensagem;
     public TextMeshProUGUI messageLove;  //Botão a ser pressionado
 
